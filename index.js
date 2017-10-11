69dd5db05900ff4a3635bddeadba26af26e6ff3d
Jaco Greeff <jaco@parity.io>
2017-10-11 09:33:45 +0200

    Add engines field

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-if (typeof module !== 'undefined' && module.exports) {
+if (typeof module !== 'undefined' && typeof fetch === 'undefined') {
   global.fetch = require('node-fetch');
 }
 
