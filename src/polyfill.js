5f6a6ff5f49dbda45aa6c84b88a11020a9d20898
Jaco Greeff <jaco@parity.io>
2017-11-06 12:13:06 +0100

    Polyfill Promise before fetch

--- src/polyfill.js
+++ src/polyfill.js
@@ -14,10 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-if (typeof fetch === 'undefined') {
-  require('isomorphic-fetch');
-}
-
 if (typeof Promise === 'undefined') {
   require('es6-promise').polyfill();
+}
+
+if (typeof fetch === 'undefined') {
+  require('isomorphic-fetch');
 }
