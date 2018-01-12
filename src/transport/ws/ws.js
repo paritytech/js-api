0d53b44cbad5e6c0bce6626ecc0b8742e88acb32
Jaco Greeff <jacogr@gmail.com>
2018-01-12 13:35:15 +0100

    Add polyfill for WebSokcet on node (#27)

--- src/transport/ws/ws.js
+++ src/transport/ws/ws.js
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+require('./polyfill');
+
 const { keccak_256 } = require('js-sha3'); // eslint-disable-line camelcase
 
 const { Logging } = require('../../subscriptions');
